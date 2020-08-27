d0e0f73c6e33b0bcb8debf30aa816c918423a2c6
Laura Izabel <lauraizabel2563@gmail.com>
2020-08-27 15:13:08 -0300

    add gitapi and dark theme

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import {
   Container,
@@ -15,16 +16,58 @@ import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
 import RandomCalendar from "../../components/RandomCalendar";
 
+import { APIUser, APIRepo } from "../../@types";
+
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
+
 const Profile: React.FC = () => {
-  const TabContent = () => {
-    return (
-      <div className="content">
-        <RepoIcon />
-        <span className="label">Repositories</span>
-        <span className="number">6</span>
-      </div>
-    );
-  };
+  const { username = "laaura87" } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async (responses) => {
+      const [userResponse, reposResponse] = responses;
+
+      if (userResponse.status === 404) {
+        setData({ error: "User not found!" });
+        return;
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.5 - Math.random());
+      const slicedRepos = shuffledRepos.slice(0, 6); // 6 repos
+
+      setData({
+        user,
+        repos: slicedRepos,
+      });
+    });
+  }, [username]);
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>;
+  }
+
+  if (!data?.user || !data?.repos) {
+    return <h1>Loading...</h1>;
+  }
+
+  const TabContent = () => (
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">{data.user?.public_repos}</span>
+    </div>
+  );
 
   return (
     <Container>
@@ -33,52 +76,50 @@ const Profile: React.FC = () => {
           <span className="offset" />
           <TabContent />
         </div>
+
         <span className="line" />
       </Tab>
 
       <Main>
         <LeftSide>
           <ProfileData
-            username={"laaura87"}
-            name={"Laura Izabel"}
-            avatarUrl={"https://avatars0.githubusercontent.com/u/40806730?v=4"}
-            followers={7}
-            following={7}
-            company={"never"}
-            location={"Caruaru, Pernambuco"}
-            email={"lauraizabel2563@gmail.com"}
-            blog={undefined}
+            username={data.user.login}
+            name={data.user.name}
+            avatarUrl={data.user.avatar_url}
+            followers={data.user.followers}
+            following={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.blog}
           />
         </LeftSide>
 
         <RightSide>
           <Tab className="mobile">
             <TabContent />
-            <span className="line"></span>
+            <span className="line" />
           </Tab>
+
           <Repos>
-            <h2>Random Repos</h2>
+            <h2>Random repos</h2>
 
             <div>
-              {[1, 2, 3, 4, 5, 6].map((n) => {
-                return (
-                  <RepoCard
-                    key={n}
-                    username={"laaura87"}
-                    reponame={"yt-downloader"}
-                    description={
-                      "a website with a downloader videos of youtube"
-                    }
-                    language={"JavaScript"}
-                    stars={3}
-                    forks={2}
-                  />
-                );
-              })}
+              {data.repos.map((item) => (
+                <RepoCard
+                  key={item.name}
+                  username={item.owner.login}
+                  reponame={item.name}
+                  description={item.description}
+                  language={item.language}
+                  stars={item.stargazers_count}
+                  forks={item.forks}
+                />
+              ))}
             </div>
           </Repos>
 
-          <CalendarHeading>Random Calendar</CalendarHeading>
+          <CalendarHeading>Random calendar</CalendarHeading>
 
           <RandomCalendar />
         </RightSide>
