b360c4d16d68993e0cf73a8be382ccbc4f91de4d
Laura Izabel <lauraizabel2563@gmail.com>
2020-08-26 21:46:50 -0300

    repo page done

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  States,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/laaura87"}>
+          laaura87
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/laaura87/yt-downloader"}>
+          yt-downloader
+        </Link>
+      </Breadcrumb>
+
+      <p>a website with a downloader videos of youtube</p>
+
+      <States>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </States>
+
+      <LinkButton
+        href={"https://github.com/laaura87/downloader-youtube-nodejs"}
+      >
+        <GithubIcon />
+        <span>View On GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
